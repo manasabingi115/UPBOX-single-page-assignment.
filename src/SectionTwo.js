07ba4c0e830bdebfdf386147cbaa5d135c0635da
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-03-12 08:56:08 -0800

    added some code.

--- src/SectionTwo.js
+++ src/SectionTwo.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function SectionTwo() {
   return (
-    <div>
+    <div className="sec-two">
       <h1>Pick your plan</h1>
-      <div>
+      <div className="sec-two-div-1">
         <h1>Monthly</h1>
         <h1>$18/mo</h1>
         <button>FIND YOUR BOX</button>
       </div>
-      <div>
+      <div className="sec-two-div-2">
         <h1>Yearly</h1>
         <h1>$15/mo</h1>
         <button>FIND YOUR BOX</button>
